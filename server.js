1cf1917ce95cf2320143b7c1d52ba72927a5f74f
Tywin <tiaven1104@gmail.com>
2018-10-17 23:16:26 -0400

    allow cross origin

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ let userRouter = require('./routes/user')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
+
 app.use('/githubUserToken', githubAccessTokenRouter)
 app.use('/users', userRouter)
 
