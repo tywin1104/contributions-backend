ef92700b383e22e1844a4e8398d56f75c4816837
Tywin <tiaven1104@gmail.com>
2018-10-19 21:52:17 -0400

    utlize the cors npm library

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 let express = require('express')
 let app = express()
 let bodyParser = require('body-parser')
+let cors = require('cors')
 
 let githubAccessTokenRouter = require('./routes/githubAccessToken')
 let userRouter = require('./routes/user')
@@ -9,11 +10,12 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  next()
-})
+app.use(cors())
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*")
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+//   next()
+// })
 
 
 app.use('/githubUserToken', githubAccessTokenRouter)
