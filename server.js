ee9cf5d8bd6824b2c3f0e0a45e7e0e3b3f70fcd9
Tywin <tiaven1104@gmail.com>
2018-10-20 14:28:33 -0400

    prevent google healthy check fail:

--- server.js
+++ server.js
@@ -14,9 +14,12 @@ app.use(cors())
 // app.use(function (req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*")
 //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-//   next()
+//   next(）
 // })
 
+app.get('/', function(req, res) {
+  res.send('It is healthy!');
+})
 
 app.use('/githubUserToken', githubAccessTokenRouter)
 app.use('/users', userRouter)
