89e9fb2e3e001e5ec95f68d2e202b0924dd5c18e
Tywin <tiaven1104@gmail.com>
2018-10-22 21:28:58 -0400

    add config -> move to private repo:

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // Autho 0
+  client_id: 'Yc9XODv7r5sGuG6dFfzFPsg041rsIwwi',
+  client_secret: 'vGsgda6DkrnUFPu2kqUlb3FM0g7oRKGJVRJAkNNP5e9eegKg24OX_C2GqHx7XMP0',
+  // Mongodb Atlas
+  db_url: "mongodb+srv://tywin1104:13141314a@gettingstarted-rb0gy.gcp.mongodb.net/test?retryWrites=true"
+};
